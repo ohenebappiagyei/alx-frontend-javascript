a667aa0e1a157d0b7936f3e7f6d366a5426d84ca
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-23 23:07:08 +0000

    0-promote fixed

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,5 +1,5 @@
 export default function getResponseFromAPI() {
-	return new Promise((resolve, reject) => {
-		resolve("API response data");
-	});
+  return new Promise((resolve) => {
+    resolve('API response data');
+  });
 }
