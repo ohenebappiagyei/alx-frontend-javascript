81932d8c2cb769a460c260affe2d8f8dc63c39f0
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-23 23:11:42 +0000

    2 is fixed

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,13 +1,13 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then((response) => {
+    .then(() => {
       console.log('Got a response from the API');
       return {
         status: 200,
         body: 'success',
       };
     })
-    .catch((error) => {
+    .catch(() => {
       console.log('Got a response from the API');
       return new Error();
     });
