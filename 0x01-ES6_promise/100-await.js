e8eeff6bf5359f4a94e8198f57d10b611a486bb5
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-24 09:17:06 +0000

    100 done

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from './utils.js';
+import { uploadPhoto, createUser } from './utils';
 
 async function asyncUploadUser() {
   try {
@@ -6,12 +6,12 @@ async function asyncUploadUser() {
 
     return {
       photo,
-      user
+      user,
     };
   } catch (error) {
     return {
       photo: null,
-      user: null
+      user: null,
     };
   }
 }
