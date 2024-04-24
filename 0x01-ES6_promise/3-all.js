bef2298c21ca49de42da3d93ac550ab3fd97ee82
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-24 07:06:56 +0000

    2 fixed

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
-import { uploadPhoto, createUser } from "./utils.js";
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photoResponse, userResponse]) => {
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error('Signup system offline');
     });
 }
