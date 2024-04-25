7ddd264268c6a04b7be66808584af836f0138554
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-25 17:41:04 +0000

    check 6 again

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,19 +3,17 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    if (typeof floors !== 'number') {
-      throw new TypeError('FLoors must be a number');
-    }
     // eslint-disable-next-line no-underscore-dangle
     this._floors = floors;
   }
 
-  get floors() {
-    // eslint-disable-next-line no-underscore-dangle
-    return this._floors;
-  }
+  // Getter for floors
+  // eslint-disable-next-line no-underscore-dangle
+  get floors() { return this._floors; }
 
+  // Override evacuationWarningMessage method
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors.`;
+    // eslint-disable-next-line no-underscore-dangle
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
