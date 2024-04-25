7a90e0456288024d6b59463cdcf1afe4c29d92d9
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-25 17:04:27 +0000

    try 5 again

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,16 @@
-export default class Building {
-  constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new TypeError('Sqft must be a number');
-    }
-    // eslint-disable-next-line no-underscore-dangle
-    this._sqft = sqft;
-  }
+class Building {
+  // eslint-disable-next-line no-underscore-dangle
+  constructor(sqft) { this._sqft = sqft; }
 
+  // Getter for sqft
   // eslint-disable-next-line no-underscore-dangle
   get sqft() { return this._sqft; }
 
+  // Abstract method to be overridden by subclasses
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
+
+export default Building;
