c71700f65c086bd0f37f9ebbd229c16a794674ed
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-26 00:06:28 +0000

    check 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,30 +1,15 @@
-export class Car {
-    constructor(brand, motor, color) {
-      // Define private properties using Symbol
-      this[_brandSymbol] = brand;
-      this[_motorSymbol] = motor;
-      this[_colorSymbol] = color;
-    }
-  
-    get brand() {
-      return this[_brandSymbol];
-    }
-  
-    get motor() {
-      return this[_motorSymbol];
-    }
-  
-    get color() {
-      return this[_colorSymbol];
-    }
-  
-    cloneCar() {
-      // Create a new Car instance with the same properties
-      return new Car(this.brand, this.motor, this.color);
-    }
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
   }
-  
-  // Define private property symbols
-  const _brandSymbol = Symbol('brand');
-  const _motorSymbol = Symbol('motor');
-  const _colorSymbol = Symbol('color');
+
+  static get [Symbol.species]() {
+    return this.prototype.constructor;
+  }
+
+  cloneCar() {
+    return new (this.constructor[Symbol.species])();
+  }
+}
