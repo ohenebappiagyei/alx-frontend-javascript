9abcb33f38a7a5be4484c70a8437ba55dc2e64d0
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-29 21:33:31 +0000

    check 9

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+function groceriesList() {
+  const groceries = [
+    { name: "Apples", quantity: 10 },
+    { name: "Tomatoes", quantity: 10 },
+    { name: "Pasta", quantity: 1 },
+    { name: "Rice", quantity: 1 },
+    { name: "Banana", quantity: 5 },
+  ];
+
+  return new Map(groceries.map((item) => [item.name, item.quantity]));
+}
+
+export default groceriesList;
