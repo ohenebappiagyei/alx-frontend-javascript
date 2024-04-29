821a6493c0328ed8b7eb1192e0a3120578695289
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-29 20:03:39 +0000

    filter done check

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,3 +1,3 @@
-export default function getStudentsByLocation(students, city) {
-	return students.filter((student) => student.location === city);
+export default function getStudentsByLocation(array, city) {
+  return array.filter((el) => el.location === city);
 }
