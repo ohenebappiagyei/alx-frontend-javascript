8ad283e098c46f1ea3a1c7fee2666c4cc9c6c17a
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-30 00:24:34 +0000

    10 is fine

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
 export default function updateUniqueItems(groceriesMap) {
-    for (let [key, value] of groceriesMap.entries()) {
-        if (value === 1) {
-            groceriesMap.set(key, 100);
-        }
+  for (const [key, value] of groceriesMap.entries()) {
+    if (value === 1) {
+      groceriesMap.set(key, 100);
     }
+  }
 }
