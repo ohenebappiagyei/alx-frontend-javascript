e79435ccaea0643f1c74db0be6eb62e20956c085
Dr. Appiagyei <ohenebappiagyei@gmail.com>
2024-04-30 21:52:25 +0000

    ambiient done

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,6 @@
+// Import types from interface.ts
+import { RowID, RowElement } from './interface';
+
+export function insertRow(row: RowElement): RowID;
+export function deleteRow(rowId: RowID): void;
+export function updateRow(rowId: RowID, row: RowElement): RowID;
